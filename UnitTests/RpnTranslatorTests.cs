09b31f29905138690a298e84e77ca21f0d5b3c33
Nafikov Ruslan <nafikovri@gmail.com>
2016-02-22 14:59:09 +0500

    Написан тест на проверку корректности возвращаемого результата метода RpnTranslator.Translate(string expression)

--- UnitTests/RpnTranslatorTests.cs
+++ UnitTests/RpnTranslatorTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 
 using NUnit.Framework;
+
+using ReversePolishNotation;
 
 namespace UnitTests
 {
@@ -10,7 +13,40 @@ namespace UnitTests
 		[Test]
 		public void Translate_Correct()
 		{
-			throw new NotImplementedException();
+			var translator = new RpnTranslator();
+
+			CollectionAssert.AreEqual(
+				translator.Translate("4+2*7"),
+				new List<IRpnElement>()
+				{
+					new RpnNumber(4),
+					new RpnNumber(2),
+					new RpnNumber(7),
+					new RpnBinaryOperator(RpnTranslator.Multiplication),
+					new RpnBinaryOperator(RpnTranslator.Addition)
+				});
+
+			CollectionAssert.AreEqual(
+				translator.Translate("3*(6-2)"),
+				new List<IRpnElement>()
+				{
+					new RpnNumber(3),
+					new RpnNumber(6),
+					new RpnNumber(2),
+					new RpnBinaryOperator(RpnTranslator.Subtraction),
+					new RpnBinaryOperator(RpnTranslator.Multiplication)
+				});
+
+			CollectionAssert.AreEqual(
+				translator.Translate("3-1/2"),
+				new List<IRpnElement>()
+				{
+					new RpnNumber(3),
+					new RpnNumber(1),
+					new RpnNumber(2),
+					new RpnBinaryOperator(RpnTranslator.Division),
+					new RpnBinaryOperator(RpnTranslator.Subtraction)
+				});
 		}
 
 		[Test]
