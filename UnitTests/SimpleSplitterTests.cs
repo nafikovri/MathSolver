18ca73fdbf19a9e9e8ba9f25676c57c1f72ec733
Nafikov Ruslan <nafikovri@gmail.com>
2016-02-26 15:03:26 +0500

    Добавлен тест для SimpleSplitter.Split(string expression).

---
+++ UnitTests/SimpleSplitterTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+using ReversePolishNotation;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class SimpleSplitterTests
+	{
+		[Test]
+		public void Split_Correct()
+		{
+			var splitter = new SimpleSplitter();
+
+			CollectionAssert.AreEqual(
+				new List<string>() {"1", "+", "2"},
+				splitter.Split("1+2"));
+
+			// выражение некорректное, но это неважно (только разделяем)
+			CollectionAssert.AreEqual(
+				new List<string>() {"(", "3", "*", "(", "2", "-", "8", ")" },
+				splitter.Split("(3*(2-8)"));
+		}
+	}
+}
